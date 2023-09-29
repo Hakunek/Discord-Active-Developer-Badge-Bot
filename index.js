f7cb0db7683142bfc31b1653f7a85dbcf4d58f93
Hakunek <hakunekpl@gmail.com>
2023-09-29 22:06:43 +0200

    Converted to ESM
    sorry
    I like it better than CJS
    😜

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-require("dotenv").config({ path: `${__dirname}/.env` });
-const { Client } = require("discord.js");
+(await import("dotenv")).config({ path: `${process.cwd()}\\\\.env` });
+import { Client, InteractionType } from "discord.js";
 const client = new Client({ intents: 0 });
 const sleep = (ms) => new Promise((r) => setTimeout(r, ms));
 console.log(`Booting up`);
 client.on("ready", async () => {
     await sleep(500);
-    console.log(`${client.user.tag}->${client.user.id}`);
+
+    console.log(`${client.user.username}->${client.user.id}`);
     console.log(`Invite URL: https://discordapp.com/oauth2/authorize?client_id=${client.user.id}&scope=applications.commands+bot&permissions=8`);
     await client.application.commands.set([{ name: "refresh", description: "You need to use this command at least once a month to keep the badge" }]);
 });
